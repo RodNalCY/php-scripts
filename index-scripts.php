edbe27c55b77ab29df6230b4f6f0a5a02d0f1744
RodNalCY <crodnal07@gmail.com>
2025-01-21 10:17:58 -0500

    Finish | numeros primos

--- index-scripts.php
+++ index-scripts.php
@@ -26,19 +26,49 @@ function modulo($a, $b)
     return $a % $b;
 }
 
+function potencia($a, $b)
+{
+    return $a ** $b;
+}
+
 // Ejemplo de uso
 $num1 = 5;
-$num2 = 6;
+$num2 = 2;
 
-$resultado = [
-    'valor-a' => $num1,
-    'valor-b' => $num2,
-    'suma' => sumar($num1, $num2),
-    'resta' => restar($num1, $num2),
+$resultado1 = [
+    'valor-a'     => $num1,
+    'valor-b'     => $num2,
+    'suma'        => sumar($num1, $num2),
+    'resta'       => restar($num1, $num2),
     'multiplicar' => multiplicar($num1, $num2),
-    'dividir' => dividir($num1, $num2),
-    'modulo' => modulo($num1, $num2),
+    'dividir'     => dividir($num1, $num2),
+    'modulo'      => modulo($num1, $num2),
+    'potencia'    => potencia($num1, $num2),
 ];
 
-echo json_encode($resultado, JSON_PRETTY_PRINT);
+// echo json_encode($resultado1, JSON_PRETTY_PRINT);
 // echo "La suma de $num1 y $num2 es: $resultado";
+
+// Funcion para obtener los numero primos
+// $primos   = [];
+// $noprimos = [];
+
+function primos($a, $b)
+{
+    $numeros  = range($a, $b);
+    foreach ($numeros as $primo) {
+        if ($primo % 2 != 0) {
+            $primos[] = $primo;
+        } else {
+            $noprimos[] = $primo;
+        }
+    }
+
+    return $primos;
+}
+
+$resultado2 = [
+    'primos'   => primos(1, 100),
+];
+
+echo json_encode($resultado2, JSON_PRETTY_PRINT);
